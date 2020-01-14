bb1f763f8a916ff2b41106dac41bb054e8768864
srikanth <srikanthadirala03@gmail.com>
2020-01-14 23:43:14 +0530

    notes

---
+++ node/playground.js
@@ -0,0 +1,25 @@
+//1. question
+// function solution(S, K) {
+//     let strings = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+//     let weekIndex = strings.findIndex((week)=>{
+//         return week.toLowerCase() === S.toLowerCase()
+//     })//2
+//     if(K>=0 && K<=500) {
+//         const finalIndex = weekIndex + K  
+//         return strings[finalIndex] 
+//     }
+// }
+// console.log(solution("Wed",2)) //fri
+// console.log(solution("Fri",2)) //sun
+// console.log(solution("Mon",2)) //wed
+
+//2. question
+// 268  -- 5
+// 5268
+// 9995
+
+function number(k,S) {
+
+}
+
+number(268,5)
